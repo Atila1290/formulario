3177d2b43e9c1e56f7e0ac1aa27a808d07310c52
Átila <atilasilva3011@gmail.com>
2024-01-28 03:55:12 -0200

    primeiro commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var campos = document.querySelectorAll(".input");
+
+const botao = document.querySelector('.btn-enviar');
+botao.addEventListener("click", () => {
+    validarDados();
+});
+
+function validarDados() {
+    let campoObrigatorio = document.querySelectorAll(".campo-obrigatorio");
+
+    for (let i = 0; i < campos.length; i++) {
+        if (campos[i].value.trim() === "") {
+            campos[i].classList.add("vazio");
+            campoObrigatorio[i].classList.add("campo-vazio");
+        } else if(campos[i].value.trim() !== "") {
+            campos[i].classList.remove("vazio");
+            campoObrigatorio[i].classList.remove("campo-vazio");
+            campos[i].classList.add("preenchido");
+        }
+    }
+}
